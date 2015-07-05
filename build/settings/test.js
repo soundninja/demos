07490ebb78df86b94bda3aeb60eb0603bb04cf24
Ross <dyson.ross@gmail.com>
2015-07-04 23:04:12 -0700

    sn OPs as a file

---
+++ build/settings/test.js
@@ -0,0 +1,8 @@
+(function (jQuery) {
+    var SoundninjaOpts = {
+        selector: '.blog-content',
+        selectorFullMatch: false,
+        minHotttnesss: 0.6707,
+        minFamiliarity: 0.6707
+    }
+});
