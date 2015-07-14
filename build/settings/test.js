2150c93fce8f0d8b116e0878909d76fb62bd323c
Ross <dyson.ross@gmail.com>
2015-07-14 03:57:41 -0700

    testing SN double selector and adding css fixes

--- build/settings/test.js
+++ build/settings/test.js
@@ -1,5 +1,5 @@
 var SoundninjaOpts = {
-    selector: '.entry-content p',
+    selector: '.entry-content p, .entry-summary',
     selectorFullMatch: false,
     minHotttnesss: 0.6707,
     minFamiliarity: 0.6707
