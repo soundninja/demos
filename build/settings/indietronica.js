cbd0ef583866d5e5d2a0d9bb3c6a551737ec703d
Ross Dyson <dyson.ross@gmail.com>
2015-08-18 02:47:26 -0700

    trying fast load 1

--- build/settings/indietronica.js
+++ build/settings/indietronica.js
@@ -2,5 +2,6 @@ var SoundninjaOpts = {
     selector: '.entry-content p',
     selectorFullMatch: false,
     minHotttnesss: 0.6707,
-    minFamiliarity: 0.6707
+    minFamiliarity: 0.6707,
+    fastLoad: true
 };
