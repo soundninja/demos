a6c3aac103199c406cef36080111ac90d42b40bb
Ross Dyson <dyson.ross@gmail.com>
2015-10-14 13:18:14 -0700

    nashville gab mock demo

---
+++ NashvilleGab-demo/NVB_files/page_data/e9S_Ok3V0u-.js
@@ -0,0 +1,5 @@
+/*!CK:1571976974!*//*1444807167,*/
+
+if (self.CavalryLogger) { CavalryLogger.start_js(["9Coms"]); }
+
+__d("PageCallToActionRef",[],function a(b,c,d,e,f,g){c.__markCompiled&&c.__markCompiled();f.exports={NONE:"None",MOBILE_PAGE_PRESENCE_CALL_TO_ACTION:"mobile_page_presence_call_to_action",COVER_PHOTO_SURFACE:"coverPhoto",HOVER_CARD_SURFACE:"hoverCard",PAGE_PLUGIN_SURFACE:"pagePlugin",ADMIN_MENU_TEST_LINK:"adminMenuTestLink",PAGE_PRESENCE_LHS_CARD:"pagePresenceLhsCard"};},null,{});
