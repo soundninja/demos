84122739d259ece0902c0f666d6eab887e081b71
Ross Dyson <dyson.ross@gmail.com>
2015-12-07 18:37:07 -0800

    new God is in the TV demo

---
+++ Godisinthetv-demo/godisinthetvzine_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"66198 http:\/\/www.godisinthetvzine.co.uk\/?p=66198","comments":0}]});
+}
