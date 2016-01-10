ce1625196d31891b5f15f9e02889c4a0b2ed1126
Ross Dyson <ross@soundninja.com>
2016-01-10 14:40:02 -0800

    live demo settings for BBB, Eargazm, Super45 and Travis Faulk

---
+++ build/settings/super45.js
@@ -0,0 +1,6 @@
+var SoundninjaOpts = {
+    selector: '.post_content p',
+    selectorFullMatch: false,
+    minHotttnesss: 0.6707,
+    minFamiliarity: 0.6707
+};
---
+++ build/settings/travisfaulk.js
@@ -0,0 +1,6 @@
+var SoundninjaOpts = {
+    selector: '.post-single p',
+    selectorFullMatch: false,
+    minHotttnesss: 0.6707,
+    minFamiliarity: 0.6707
+};
